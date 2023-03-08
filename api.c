063d52d2b258d34b5452004c94f40d1f6e1e2ed1
langares <langares@yahoo.es>
2023-03-08 11:07:57 +0100

    Update api.c

--- api.c
+++ api.c
@@ -1,9 +1,9 @@
 #include "include.h"
 #include "api.h"
 
-#define SYSTEM   & ( DarkenAPI_deSystem_t  ) { de_system_init,  de_system_update,    de_system_end    }
-#define MANAGER  & ( DarkenAPI_deManager_t ) { de_manager_init, de_manager_update,   de_manager_end   }
-#define ENTITY   & ( DarkenAPI_deEntity_t  ) { de_entity_new,   de_entity_set_state, de_entity_delete }
+#define SYSTEM   & ( DarkenAPI_deSystem_t  ) { deSystem_init,  deSystem_update,    deSystem_end    }
+#define MANAGER  & ( DarkenAPI_deManager_t ) { deManager_init, deManager_update,   deManager_end   }
+#define ENTITY   & ( DarkenAPI_deEntity_t  ) { deEntity_new,   deEntity_set_state, deEntity_delete }
 
 const DarkenAPI         *const $  = &(DarkenAPI) { SYSTEM, MANAGER, ENTITY, darken };
 const DarkenAPI_deSystem_t  *const $s = SYSTEM;
