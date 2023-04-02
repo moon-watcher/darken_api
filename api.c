f330715d7bdc9bb0e71e4316ede6f1e41ea50484
langares <langares@yahoo.es>
2023-04-02 12:45:53 +0200

    Update api.c

--- api.c
+++ api.c
@@ -1,7 +1,7 @@
 #include "api.h"
 
-const deSystemAPI  *const deSystem  = & ( deSystemAPI  ) { deSystem_init,  deSystem_update,  deSystem_end,   deSystem_add };
-const deManagerAPI *const deManager = & ( deManagerAPI ) { deManager_init, deManager_update, deManager_end   };
+const deSystemAPI  *const deSystem  = & ( deSystemAPI  ) { deSystem_init,  deSystem_update,  deSystem_end,    deSystem_add                   };
+const deManagerAPI *const deManager = & ( deManagerAPI ) { deManager_init, deManager_update, deManager_end                                   };
 const deEntityAPI  *const deEntity  = & ( deEntityAPI  ) { deEntity_new,   deEntity_update,  deEntity_change, deEntity_jump, deEntity_delete };
-const deStateAPI   *const deState   = & ( deStateAPI   ) { deState_exec,   deState_enter,    deState_update, deState_leave };
-const DarkenAPI    *const darkEn    = & ( DarkenAPI    ) { darken, darken_end };
+const deStateAPI   *const deState   = & ( deStateAPI   ) { deState_exec,   deState_enter,    deState_update,  deState_leave                  };
+const DarkenAPI    *const darkEn    = & ( DarkenAPI    ) { darken,         darken_end                                                        };
